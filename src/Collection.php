696d93c00d2872d6516a95b028faf3b8add3eabd
Willem Leuverink <willem@leuver.ink>
2021-09-15 17:14:19 +0200

    implement ArrayAccess

--- src/Collection.php
+++ src/Collection.php
@@ -2,19 +2,37 @@
 
 namespace Leuverink\Collections;
 
+use ArrayAccess;
 use Countable;
 
-class Collection implements Countable
+class Collection implements Countable, ArrayAccess
 {
     public function __construct(protected array $items = []) { }
 
     public static function make($items = [])
     {
-        $items = is_array($items) ? $items : [$items];
-        return new static($items);
+        return new static(
+            is_array($items) ? $items : [$items]
+        );
     }
 
     public function count() {
         return count($this->items);
+    }
+
+    public function offsetExists($offset) {
+        return isset($this->items[$offset]);
+    }
+
+    public function offsetGet($offset) {
+        return $this->items[$offset];
+    }
+
+    public function offsetSet($offset, $value) {
+        $this->items[$offset] = $value;
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->items[$offset]);
     }
 }
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -13,3 +13,7 @@ it('wraps a single argument in an array if it is not already an array')
 it('wraps an array of items')
     ->expect(fn () => Collection::make(['foo', 'bar', 'baz']))
     ->toHaveCount(3);
+
+it('acts as an array')
+    ->expect(fn () => Collection::make('foo')[0])
+    ->toBe('foo');
