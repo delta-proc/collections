76a7369bf6ac271d9345bddf259c3555624ace5b
Willem Leuverink <willem@leuver.ink>
2021-09-15 18:21:55 +0200

    wip

--- tests/MapTest.php
+++ tests/MapTest.php
@@ -4,10 +4,10 @@ use Leuverink\Collections\Collection;
 
 it('maps into a new datastructure')
     ->expect(
-        fn () => Collection::make([1, 2, 3])->map(fn ($number) => $number * 2)
+        Collection::make([1, 2, 3])->map(fn ($number, $key) => $number * $key)
     )
     ->sequence(
+        fn ($number) => $number->toBe(0),
         fn ($number) => $number->toBe(2),
-        fn ($number) => $number->toBe(4),
         fn ($number) => $number->toBe(6)
     );
