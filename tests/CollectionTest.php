9fb7cb1cc0f4ec1590e4449b4d31d84fd127ba90
Willem Leuverink <willem@leuver.ink>
2021-09-16 14:28:03 +0200

    cleanup tests

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -3,27 +3,27 @@
 use Leuverink\Collections\Collection;
 
 it('initalizes as an empty collection when no arguments are passed')
-    ->expect(fn () => Collection::make())
+    ->expect(Collection::make())
     ->toHaveCount(0);
 
 
 it('wraps a single argument in an array if it is not already an array')
-    ->expect(fn () => Collection::make('foo'))
+    ->expect(Collection::make('foo'))
     ->toHaveCount(1);
 
 
 it('wraps an array of items')
-    ->expect(fn () => Collection::make(['foo', 'bar', 'baz']))
+    ->expect(Collection::make(['foo', 'bar', 'baz']))
     ->toHaveCount(3);
 
 
 it('acts as an array')
-    ->expect(fn () => Collection::make('foo')[0])
+    ->expect(Collection::make('foo')[0])
     ->toBe('foo');
 
 
 it('is iterable')
-    ->expect(fn () => Collection::make(['foo', 'bar', 'baz']))
+    ->expect(Collection::make(['foo', 'bar', 'baz']))
     ->sequence(
         fn ($number) => $number->toBe('foo'),
         fn ($number) => $number->toBe('bar'),
